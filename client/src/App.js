adf0fb1514d412fc9e642696e6369473cf06dba3
Josue Peralta <jperal77@gmail.com>
2019-02-22 23:47:37 -0600

    setup client routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
+import Chat from './components/Chat';
 import { Button } from 'antd';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/chat" component={Chat} />
         <Button type="primary">Primary</Button>
       </div>
     );
