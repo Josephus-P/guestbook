55630b1d449c7a6605a691f4e1a3c135f617e3e3
Josue Peralta <jperal77@gmail.com>
2019-02-24 12:25:37 -0600

    added header

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import { Row, Col, Card, Button, Tabs, List, Comment, Form, Input } from 'antd';
+import {
+  Row,
+  Col,
+  Card,
+  Button,
+  Tabs,
+  List,
+  Comment,
+  Form,
+  Input,
+  Layout,
+} from 'antd';
 import './chat.scss';
 
 const TabPane = Tabs.TabPane;
 const TextArea = Input.TextArea;
 const messages = [{ comment: 'Hello World' }];
+const { Header, Content } = Layout;
 
 class Chat extends Component {
   state = {
@@ -15,40 +27,47 @@ class Chat extends Component {
     const { messages } = this.state;
 
     return (
-      <div className="chat">
-        <Row type="flex" justify="center">
-          <Col xs={24} md={20} xl={16} xxl={12}>
-            <Card>
-              <Tabs tabPosition="top" defaultActiveKey="2" size="large">
-                <TabPane tab="Online Users" key="1">
-                  Online
-                </TabPane>
-                <TabPane tab="General" key="2">
-                  <Row className="tab-general">
-                    <Col xs={24}>
-                      <List
-                        className="list"
-                        dataSource={messages}
-                        renderItem={message => (
-                          <Comment content={<p>{message.comment}</p>} />
-                        )}
-                      />
-                    </Col>
-                    <Col xs={24}>
-                      <Form.Item>
-                        <TextArea rows={2} />
-                      </Form.Item>
-                      <Form.Item>
-                        <Button htmlType="submit">Add Comment</Button>
-                      </Form.Item>
-                    </Col>
-                  </Row>
-                </TabPane>
-              </Tabs>
-            </Card>
-          </Col>
-        </Row>
-      </div>
+      <Layout className="chat">
+        <Header className="header">
+          <h2>Karma Chat</h2>
+          <p>Welcome, Guest</p>
+          <Button>Sign Out</Button>
+        </Header>
+        <Content className="content">
+          <Row type="flex" justify="center">
+            <Col xs={24} md={20} xl={16} xxl={12}>
+              <Card>
+                <Tabs tabPosition="top" defaultActiveKey="2" size="large">
+                  <TabPane tab="Online Users" key="1">
+                    Online
+                  </TabPane>
+                  <TabPane tab="General" key="2">
+                    <Row className="tab-general">
+                      <Col xs={24}>
+                        <List
+                          className="list"
+                          dataSource={messages}
+                          renderItem={message => (
+                            <Comment content={<p>{message.comment}</p>} />
+                          )}
+                        />
+                      </Col>
+                      <Col xs={24}>
+                        <Form.Item>
+                          <TextArea rows={2} />
+                        </Form.Item>
+                        <Form.Item>
+                          <Button htmlType="submit">Add Comment</Button>
+                        </Form.Item>
+                      </Col>
+                    </Row>
+                  </TabPane>
+                </Tabs>
+              </Card>
+            </Col>
+          </Row>
+        </Content>
+      </Layout>
     );
   }
 }
