26624cef20076d4e8b7053612fe094b6db3ef6a9
Josue Peralta <jperal77@gmail.com>
2019-03-20 21:02:50 -0500

    Moved firebase admin setup

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const admin = require('firebase-admin');
+const admin = require('./firebase-admin/admin');
 const db = require('./db/dbConfig');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+require('dotenv').config();
 
 const onlineUsers = {};
 
 app.use(express.json());
 app.use(cors());
-
-admin.initializeApp({
-  credential: admin.credential.cert({
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url:
-      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  }),
-});
 
 // Middleware to verify the Firebase token
 async function verifyToken(req, res, next) {
